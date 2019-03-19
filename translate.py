a18b8389703ffc9ce6a8c145257f84edbd5cf025
Breanna N. Sipley <breanna.sipley@gmail.com>
2019-03-19 14:03:55 -0500

    fixing translate function to break when extra base: adding if statement with len()

--- translate.py
+++ translate.py
@@ -17,11 +17,12 @@ def translate_sequence(rna_sequence, genetic_code):
     if len(rna_sequence) >= 3:
         for triplet in range(0, len(rna_sequence), 3):
             codon=rna_sequence[triplet:triplet+3]
-            aa=genetic_code.get(codon, aa_seq)
-            if aa == '*':
-                break
-            else:
-                aa_seq+=aa
+            if len(codon) == 3:
+                aa=genetic_code.get(codon, aa_seq)
+                if aa == '*':
+                    break
+                else:
+                    aa_seq+=aa
     return aa_seq
 
 def get_all_translations(rna_sequence, genetic_code):
